894c661e66f7af58306014f50bb442d33726b7cb
AdityaMogera <aditya.moger@gmail.com>
2021-08-23 12:44:41 +0530

    first code

---
+++ firstcode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+
+            System.Console.WriteLine("welcome to mindteck");
+        }
+    }
+}
